ea218eb1cea1fd573838ef347f7a2939edcc43e3
s8-dev <g.kuznetsov@siams.com>
2021-04-14 09:23:05 +0500

    angular-blog: add firebase and token id to app

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCl-15oaQtE6qh0_eCYbwNfWgYzhAvc4nE'
 };
