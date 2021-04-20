37f882cf21743d27a05030cfca40434050237077
s8-dev <g.kuznetsov@siams.com>
2021-04-20 11:05:17 +0500

    angular-blog: create post service & FBDB

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyCl-15oaQtE6qh0_eCYbwNfWgYzhAvc4nE'
+  apiKey: 'AIzaSyCl-15oaQtE6qh0_eCYbwNfWgYzhAvc4nE',
+  fbDbUrl: 'https://angular-blog-7cdc4-default-rtdb.firebaseio.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from './interface';
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyCl-15oaQtE6qh0_eCYbwNfWgYzhAvc4nE'
+  apiKey: 'AIzaSyCl-15oaQtE6qh0_eCYbwNfWgYzhAvc4nE',
+  fbDbUrl: 'https://angular-blog-7cdc4-default-rtdb.firebaseio.com'
 };
 
 /*
