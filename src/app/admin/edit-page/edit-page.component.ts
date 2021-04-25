f9155aca97a2429f180b4587a37d0356f31fa86e
glebus <svarog96@gmail.com>
2021-04-25 11:34:07 +0500

    angular-blog: add editing for post

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,23 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
 import {PostService} from '../../shared/post.service';
 import {switchMap} from 'rxjs/operators';
 import {Post} from '../../shared/interfaces';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy {
 
   form: FormGroup
+  post: Post
+  submitted = false
+  uSub: Subscription
 
   constructor(
     private route: ActivatedRoute,
     private postService: PostService
-  ) { }
+  ) {
+  }
 
   ngOnInit(): void {
     this.route.params
@@ -26,15 +31,34 @@ export class EditPageComponent implements OnInit {
           return this.postService.getById(params['id'])
         })
       ).subscribe((post: Post) => {
-        this.form = new FormGroup({
-          title: new FormControl(post.title, Validators.required),
-          text: new FormControl(post.text, Validators.required)
-        })
+      this.post = post
+      this.form = new FormGroup({
+        title: new FormControl(post.title, Validators.required),
+        text: new FormControl(post.text, Validators.required)
+      })
     })
   }
 
-  submit() {
+  ngOnDestroy() {
+    if (this.uSub) {
+      this.uSub.unsubscribe()
+    }
+  }
 
+
+
+  submit() {
+    if (this.form.invalid) {
+      return
+    }
+
+    this.uSub = this.postService.update({
+      ...this.post,
+      text: this.form.value.text,
+      title: this.form.value.title
+    }).subscribe(() => {
+      this.submitted = false
+    })
   }
 
   isInvalid(fieldName: string): boolean {
